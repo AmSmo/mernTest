957d1dd83eedb7f184fca6f7b4aa2501d08b5f87
AmSmo <adam.smolenski@gmail.com>
2020-11-17 07:26:59 -0500

    discovering routes

--- app.js
+++ app.js
@@ -2,11 +2,21 @@ const express = require("express");
 const mongoose = require('mongoose');
 const app = express();
 const db = require('./config/keys').mongoURI
+const users = require("./routes/api/users");
+const tweets = require("./routes/api/tweets");
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 mongoose
-    .connect(db, { useNewUrlParser: true })
+    .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log("Connected to MongoDB successfully"))
     .catch(err => console.log(err));
+
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
 app.get("/", (req, res) => res.send("Hleo World"));
+app.use("/api/users", users);
+app.use("/api/tweets", tweets);
